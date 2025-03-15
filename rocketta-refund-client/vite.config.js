6e71e11e9adef8811e054c767ab3b686b9f41fe0
BrianXYZ <brianchima22@gmail.com>
2025-03-15 17:00:34 +0100

    changed api url to hosted api server

--- rocketta-refund-client/vite.config.js
+++ rocketta-refund-client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://rocketta-refund.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
