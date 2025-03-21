3f869114b77d8f5369356c3d72f7422b168a3334
BrianXYZ <brianchima22@gmail.com>
2025-03-21 07:49:59 +0100

    added html inline width and height for images to prevent layout shift onload

--- rocketta-refund-client/src/components/AboutCard.jsx
+++ rocketta-refund-client/src/components/AboutCard.jsx
@@ -5,7 +5,7 @@ const AboutCard = ({ title, image, description }) => {
         <>
             <div className="bg-white w-5/6 md:w-[26rem] xl:w-1/5 h-fit lg:h-[38rem] flex-wrap xl:flex-nowrap flex flex-col justify-between text-center border-b-4 rounded-xl border-blue-600 hover:-translate-y-3 pb-6">
                 <div>
-                    <img src={image} loading="lazy" alt="" />
+                    <img src={image} loading="lazy" width="8000px" height="300px" alt="" />
                     <h3 className="text-2xl my-3 font-bold">{title}</h3>
                     <div className="mb-3 px-2 text-sm md:text-base">
                         {description}
