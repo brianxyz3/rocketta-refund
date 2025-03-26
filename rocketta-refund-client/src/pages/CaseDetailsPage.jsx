7145a6c93bafd28b36462f9ffbad729b5ed0a78a
BrianXYZ <brianchima22@gmail.com>
2025-03-26 10:47:03 +0100

    added conditional rendering for admin use and client use respectively

--- rocketta-refund-client/src/pages/CaseDetailsPage.jsx
+++ rocketta-refund-client/src/pages/CaseDetailsPage.jsx
@@ -11,11 +11,11 @@ import Loader from "../components/Loader";
 const CaseDetailsPage = () => {
     const {id} = useParams();
     const navigate = useNavigate();
-    const {currentUser} = useAuth();
+    const {currentUser, userLoggedIn} = useAuth();
     const [fileData, setFileData] = useState([]);
     const [comments, setComments] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [isUpdatingApi, setIsUpdatingApi] = useState(false);
+    const [isUpdatingServer, setIsUpdatingServer] = useState(false);
 
     const headerObj = {
         authorization: currentUser.token,
@@ -24,8 +24,8 @@ const CaseDetailsPage = () => {
         "Content-Type": "application/json",
     }
 
-    useEffect(() => {        
-        if(!currentUser.isAdmin) navigate("/");
+    currentUser.isAdmin && useEffect(() => {        
+        if (!userLoggedIn) navigate("/");
         const getFile = async () => {
             try{
             const res = await getFileData(id, headerObj);
@@ -40,7 +40,6 @@ const CaseDetailsPage = () => {
         getFile();
     }, []);
 
-    
 
     const updateComments = (newComment) => {
         setComments((currData) => (
@@ -49,7 +48,7 @@ const CaseDetailsPage = () => {
     }
     
     const openInvestigation = async () => {
-        setIsUpdatingApi(true);
+        setIsUpdatingServer(true);
         try{
         const res = await updateCaseFile(id, {isActiveInvestigation: true}, headerObj).catch((err) => console.log(err));
         if(!res.status == 200) toast.error("Something went wrong. Try again.");
@@ -60,12 +59,12 @@ const CaseDetailsPage = () => {
         } catch (err) {
             return console.log(err);
         } finally {
-            setIsUpdatingApi(false);
+            setIsUpdatingServer(false);
         }
     }
    
     const closeCase = async () => {
-        setIsUpdatingApi(true);
+        setIsUpdatingServer(true);
         try{
             const res = await updateCaseFile(id, { isActiveInvestigation: false, isClosed: true }, headerObj).catch((err) => console.log(err));
             if (!res.status == 200) toast.error("Something went wrong. Try again.");
@@ -76,13 +75,13 @@ const CaseDetailsPage = () => {
         } catch(err) {
             return console.log(err);
         } finally {
-            setIsUpdatingApi(false);
+            setIsUpdatingServer(false);
         }
         
     }
     
     const reOpenCase = async () => {
-        setIsUpdatingApi(true);
+        setIsUpdatingServer(true);
         try{
             const res = await updateCaseFile(id, { isActiveInvestigation: true, isClosed: false }, headerObj).catch((err) => console.log(err));
             if (!res.status == 200) toast.error("Something went wrong. Try again.");
@@ -93,18 +92,18 @@ const CaseDetailsPage = () => {
         } catch(err) {
             return console.log(err);
         } finally {
-            setIsUpdatingApi(false);
+            setIsUpdatingServer(false);
         }
     }
     
   return (
     <main>
-        {currentUser.isAdmin &&
+          {currentUser.isAdmin || userLoggedIn &&
         <>
       <section>
             <div className="container m-auto py-6 px-6">
                 <Link
-                    to="/cases"
+                    to={`${currentUser.isAdmin ? "/cases" : "/userHistory"}`}
                     className="text-indigo-500 hover:text-indigo-600 flex items-center hover:scale-x-105 duration-200"
                 >
                     <ArrowBack className="mr-2" /> Back to All Case Files
@@ -122,10 +121,11 @@ const CaseDetailsPage = () => {
                             <h1 className="text-xl md:text-2xl text-[#0f3bc0] font-bold mb-4">
                                 Client's Personal Details
                             </h1>
+
                             <div className="text-gray-500 text-sm md:text-base mb-2">First Name: <span className="text-black inline-block text-base md:text-lg">{fileData.firstName}</span></div>
-                              <div className="text-gray-500 text-sm md:text-base mb-2">Last Name: <span className="text-black inline-block text-base md:text-lg">{fileData.lastName}</span></div>
-                              <div className="text-gray-500 text-sm md:text-base mb-2">Contact Email: <span className="text-black inline-block text-base text-wrap md:text-lg">{fileData.contactEmail}</span></div>
-                              <div className="text-gray-500 text-sm md:text-base mb-2">Contact Phone: <span className="text-black inline-block text-base md:text-lg">{fileData.contactPhone || "Nil"}</span></div>
+                            <div className="text-gray-500 text-sm md:text-base mb-2">Last Name: <span className="text-black inline-block text-base md:text-lg">{fileData.lastName}</span></div>
+                            <div className="text-gray-500 text-sm md:text-base mb-2">Contact Email: <span className="text-black inline-block text-base text-wrap md:text-lg">{fileData.contactEmail}</span></div>
+                            <div className="text-gray-500 text-sm md:text-base mb-2">Contact Phone: <span className="text-black inline-block text-base md:text-lg">{fileData.contactPhone || "Nil"}</span></div>
                       </div>
 
                       <div className="bg-white p-3 md:p-6 rounded-lg shadow-md mt-6">
@@ -136,15 +136,15 @@ const CaseDetailsPage = () => {
                           <p className="mb-4">
                               {fileData.description || "None"}
                           </p>
-
-                              <h3 className="text-[#0f3bc0] text-lg font-bold mb-2">Amount Lost</h3>
-
+                            <h3 className="text-[#0f3bc0] text-lg font-bold mb-2">Amount Lost</h3>
                           <p className="mb-4">{fileData.lostAmount}</p>
                       </div>
                       </div>
-                        {/* Sidebar */}
-                      <aside className="bg-gray-200 rounded-lg">
-                        {isUpdatingApi ? (<Loader loading={isUpdatingApi} size={10} />) :
+
+                    {currentUser.isAdmin 
+                    // Render for Admin Use Only
+                    ? <aside className="bg-gray-200 rounded-lg">
+                        {isUpdatingServer ? (<Loader loading={isUpdatingServer} size={10} />) :
                           <div className="flex m-6 text-white font-bold gap-1 xl:gap-2 md:flex-wrap lg:flex-nowrap">
                               {!fileData.isActiveInvestigation && !fileData.isClosed && <button className="bg-green-500 w-1/2 md:w-full py-1 px-2 rounded-lg" onClick={openInvestigation}>Activate Case</button>}
                               {fileData.isActiveInvestigation && !fileData.isClosed &&  <p className="text-black text-sm bg-yellow-400 rounded-sm flex items-center justify-center p-0.5 w-full">Ongoing Investigation</p>}
@@ -154,11 +154,12 @@ const CaseDetailsPage = () => {
                             }
                         </div>
                         }
-                            {/*Add Agent's Comment  */}
+                            {/*Add Case Officer's Comment  */}
                             <div className="bg-gray-300 p-3 rounded-lg shadow-md mx-3 mt-6">
                               <CommentForm id={id} updateComments={updateComments} isActiveInvestigation={fileData.isActiveInvestigation} user={currentUser}/>
                             </div>
 
+                            {/* List Case Officer's Comments */}
                             <div className="bg-white max-h-64 overflow-auto p-2 mx-1 rounded-lg shadow-md mt-4">
                               <h2 className="text-lg my-2 text-[#0f3bc0]">Admin's Comments</h2>
                                 <ol className="flex flex-col gap-2">
@@ -168,7 +169,11 @@ const CaseDetailsPage = () => {
                                     <li className="mt-2 text-gray-500 text-xs">new update...</li>
                                 </ol>
                             </div>
+                    </aside>
+                    // Render for Client use
+                    : <aside>
                         </aside>
+                    }
                     </div>
                 </div>
             </section>
